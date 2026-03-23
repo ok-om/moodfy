3e0b11ea37f51fdba35c9c973722bdccb0ab54d3
ok-om <ok8023361@gmail.com>
2026-03-24 01:01:19 +0530

    fix: loginUser clearCookie to cookie

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -82,10 +82,11 @@ async function loginUser(req,res) {
         expiresIn: "3d"
     })
 
-   res.clearCookie("token", {
+    res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 3 * 24 * 60 * 60 * 1000
 })
 
     return res.status(200).json({
