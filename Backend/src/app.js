bc87f49c0f087c9c28756131bab7c3539bcc4271
ok-om <ok8023361@gmail.com>
2026-03-23 23:14:56 +0530

    doing npm build and modify app.js for wildcard

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,17 +2,27 @@ const express = require("express");
 const cookieParser = require('cookie-parser');
 const cors = require("cors")
 const app = express();
+const path = require("path");
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin:"http://localhost:5173",
      credentials: true
 }))
+app.use(express.static(path.join(__dirname,"../Public")));
+
 
 const authRoutes = require("./routes/auth.routes")
 const songRoutes = require("./routes/song.routes")
 
 app.use("/api/auth",authRoutes)
 app.use("/api/songs", songRoutes)
+
+
+app.get(/.*/,(req,res)=>{
+   res.sendFile(path.join(__dirname,"../Public","index.html"))
+})
 
 module.exports = app
